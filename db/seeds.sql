5e49af6bca7ae24b2b633247a876ee9b12a97f82
exumc <cordexum@gmail.com>
2018-12-19 23:04:52 -0800

    File structure and some functionality working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ("Cheeseburger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES ("Turkeyburger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES ("Bacon Burger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES ("Veggieburger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES ("BBQ Burger", false);
